62fe377e4e42d48d42aaabccacc88fa5330bbb84
Ole Martin Ruud <barskern@outlook.com>
2019-12-06 14:17:19 +0100

    Solve day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -41,10 +41,37 @@ pub fn part1(input: &str) -> Result<usize> {
     Ok(orbit_count(&m, "COM", 0))
 }
 
-// #[aoc(day6, part2)]
-// pub fn part2(input: &str) -> Result<usize> {
-//     unimplemented!()
-// }
+#[aoc(day6, part2)]
+pub fn part2(input: &str) -> Result<usize> {
+    let parents: BTreeMap<_, _> =
+        convert(input.lines().map(Ok::<&str, Box<dyn std::error::Error>>))
+            .map(|line| {
+                let mut s = line.split(')');
+                Ok((
+                    s.next().ok_or("missing center")?,
+                    s.next().ok_or("missing orbiter")?,
+                ))
+            })
+            // Swap places for center and orbiter, so the map will have the child as key and parent
+            // as value
+            .map(|(center, orbiter)| Ok((orbiter, center)))
+            .collect()?;
+
+    let mut you_parents = std::iter::successors(parents.get("YOU"), |&n| parents.get(n));
+    let san_parents = std::iter::successors(parents.get("SAN"), |&n| parents.get(n));
+
+    // Calculate the distance from YOU and SAN to the first common parent and sum the distances
+    you_parents
+        .enumerate()
+        .find_map(|(you_dist, you_parent)| {
+            san_parents
+                .clone()
+                .enumerate()
+                .find(|&(_, san_parent)| you_parent == san_parent)
+                .map(|(san_dist, _)| san_dist + you_dist)
+        })
+        .ok_or_else(|| "did not find common parent".into())
+}
 
 #[cfg(test)]
 mod tests {
@@ -58,5 +85,15 @@ mod tests {
         );
 
         assert_eq!(42, part1(input).unwrap());
+    }
+
+    #[test]
+    fn common_parent() {
+        let input = concat!(
+            "COM)B\n", "B)C\n", "C)D\n", "D)E\n", "E)F\n", "B)G\n", "G)H\n", "D)I\n", "E)J\n",
+            "J)K\n", "K)L\n", "K)YOU\n", "I)SAN",
+        );
+
+        assert_eq!(4, part2(input).unwrap());
     }
 }
