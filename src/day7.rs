3e96ea1fb5859d0c09bcdb67d3da6696ecf8dcac
Ole Martin Ruud <barskern@outlook.com>
2019-12-08 11:50:27 +0100

    Start implementing day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -2,6 +2,9 @@ use crate::intcode::Machine;
 use aoc_runner_derive::*;
 use fallible_iterator::{convert, FallibleIterator};
 use itertools::Itertools;
+use std::cell::RefCell;
+use std::io::Cursor;
+use std::io::Write;
 
 type Error = Box<dyn std::error::Error>;
 type Result<T, E = Error> = std::result::Result<T, E>;
@@ -30,10 +33,34 @@ pub fn part1(input: &str) -> Result<usize> {
         .ok_or_else(|| "no maximum output thrust".into())
 }
 
-// #[aoc(day7, part2)]
-// pub fn part2(input: &str) -> Result<usize> {
-//     unimplemented!()
-// }
+#[aoc(day7, part2)]
+pub fn part2(input: &str) -> Result<usize> {
+    let memory: Vec<isize> = input.split(',').map(|s| s.parse().unwrap()).collect();
+
+    convert((5..10).permutations(5).map(Ok::<Vec<usize>, Error>))
+        .map(|phases| {
+            let pipes: Vec<_> = phases
+                .iter()
+                .map(|phase| RefCell::new(Cursor::new(format!("{}\n", phase).into_bytes())))
+                .collect();
+
+            writeln!(pipes[0].borrow_mut(), "0")?;
+
+            pipes
+                .iter()
+                .zip(pipes.iter().cycle().skip(1))
+                .try_for_each(|(input, output)| {
+                    eprintln!("test");
+                    let (mut input, mut output) = (input.borrow_mut(), output.borrow_mut());
+                    let mut machine = Machine::new(memory.clone(), &mut *input, &mut *output);
+                    machine.run().map(|_| ())
+                })?;
+
+            unimplemented!()
+        })
+        .max()?
+        .ok_or_else(|| "no maximum output thrust".into())
+}
 
 #[cfg(test)]
 mod tests {}
