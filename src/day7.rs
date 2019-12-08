e17faa3bd7062b19f24d5057c5c167fc01a5dcce
Ole Martin Ruud <barskern@outlook.com>
2019-12-08 02:03:12 +0100

    Solve day 7 part 1

--- src/day7.rs
+++ src/day7.rs
@@ -1,11 +1,32 @@
+use crate::intcode::Machine;
 use aoc_runner_derive::*;
+use fallible_iterator::{convert, FallibleIterator};
+use itertools::Itertools;
 
 type Error = Box<dyn std::error::Error>;
 type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[aoc(day7, part1)]
 pub fn part1(input: &str) -> Result<usize> {
-    unimplemented!()
+    let memory: Vec<isize> = input.split(',').map(|s| s.parse().unwrap()).collect();
+
+    convert((0..5).permutations(5).map(Ok::<Vec<usize>, Error>))
+        .map(|phases| {
+            phases.iter().try_fold(0, |input_value, phase| {
+                // Create input and output for amplifier node
+                let input = format!("{}\n{}\n", phase, input_value);
+                let mut output = Vec::new();
+
+                let mut machine = Machine::new(memory.clone(), input.as_bytes(), &mut output);
+                machine.run()?;
+
+                String::from_utf8_lossy(&output)
+                    .parse()
+                    .map_err(Error::from)
+            })
+        })
+        .max()?
+        .ok_or_else(|| "no maximum output thrust".into())
 }
 
 // #[aoc(day7, part2)]
