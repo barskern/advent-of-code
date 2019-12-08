867119ba0670fdeb337355b274b58ff7e510cf62
Ole Martin Ruud <barskern@outlook.com>
2019-12-08 19:29:51 +0100

    Solve day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -16,10 +16,32 @@ pub fn part1(input: &str) -> Result<usize> {
         .ok_or_else(|| "no layers found".into())
 }
 
-// #[aoc(day8, part2)]
-// pub fn part2(input: &str) -> Result<usize> {
-//     unimplemented!()
-// }
+#[aoc(day8, part2)]
+pub fn part2(input: &str) -> Result<String> {
+    let image = (0..)
+        .map(|i| (i * WIDTH * HEIGHT, (i + 1) * WIDTH * HEIGHT))
+        .scan((), |_, (start, end)| input.get(start..end))
+        .fold(vec![2; WIDTH * HEIGHT], |mut image, layer| {
+            image
+                .iter_mut()
+                .zip(layer.as_bytes())
+                // only allow transparent pixels to be set by this layer (as all non-transparent
+                // pixels have been determined by a higher layer)
+                .filter(|(px, _)| **px == 2)
+                .for_each(|(px, lpx)| *px = *lpx - b'0');
+            image
+        });
+
+    Ok(image
+        .chunks_exact(WIDTH)
+        .fold(String::with_capacity((WIDTH + 1) * HEIGHT), |mut s, row| {
+            s.push('\n');
+            for px in row {
+                s.push(if *px == 1 { 'X' } else { ' ' });
+            }
+            s
+        }))
+}
 
 #[cfg(test)]
 mod tests {}
