92ca9d296807d7cf07de1edee491343682ddfbd7
Ole Martin Ruud <barskern@outlook.com>
2019-12-08 01:10:22 +0100

    Make machine generic over input and output

--- src/intcode.rs
+++ src/intcode.rs
@@ -1,14 +1,109 @@
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 use std::convert::{TryFrom, TryInto};
-use std::io::Write;
+use std::io::{BufRead, Write};
 
 type Error = Box<dyn std::error::Error>;
 type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[derive(Debug)]
-struct Machine {
+struct Machine<R, W> {
     pc: usize,
     memory: Vec<isize>,
+    input: R,
+    output: W,
+}
+
+impl<R: BufRead, W: Write> Machine<R, W> {
+    fn step(&mut self) -> Result<Status> {
+        let instr = Instruction::try_from(self.memory[self.pc])?;
+
+        // Create arguments
+        let mut args = [
+            self.pc as isize + 1,
+            self.pc as isize + 2,
+            self.pc as isize + 3,
+        ];
+        args[..instr.opcode.arg_count()]
+            .iter_mut()
+            .zip(instr.modes.iter())
+            .for_each(|(arg, mode)| match mode {
+                Mode::Position => *arg = self.memory[*arg as usize],
+                Mode::Immidiate => {}
+            });
+
+        // Update program counter
+        self.pc += instr.opcode.arg_count() + 1;
+
+        match instr.opcode {
+            Opcode::Add => {
+                let [a, b, c] = args;
+                self.memory[c as usize] = self.memory[a as usize] + self.memory[b as usize];
+                Ok(Status::Continue)
+            }
+            Opcode::Mul => {
+                let [a, b, c] = args;
+                self.memory[c as usize] = self.memory[a as usize] * self.memory[b as usize];
+                Ok(Status::Continue)
+            }
+            Opcode::Input => {
+                let [a, _, _] = args;
+                self.memory[a as usize] = {
+                    let mut s = String::new();
+                    self.input.read_line(&mut s)?;
+                    s.trim().parse()?
+                };
+                Ok(Status::Continue)
+            }
+            Opcode::Output => {
+                let [a, _, _] = args;
+                write!(self.output, "{}", self.memory[a as usize])?;
+                self.output.flush()?;
+                Ok(Status::Continue)
+            }
+            Opcode::IsNotZero => {
+                let [a, b, _] = args;
+                if self.memory[a as usize] != 0 {
+                    self.pc = self.memory[b as usize] as usize;
+                }
+                Ok(Status::Continue)
+            }
+            Opcode::IsZero => {
+                let [a, b, _] = args;
+                if self.memory[a as usize] == 0 {
+                    self.pc = self.memory[b as usize] as usize;
+                }
+                Ok(Status::Continue)
+            }
+            Opcode::LessThan => {
+                let [a, b, c] = args;
+                if self.memory[a as usize] < self.memory[b as usize] {
+                    self.memory[c as usize] = 1;
+                } else {
+                    self.memory[c as usize] = 0;
+                }
+                Ok(Status::Continue)
+            }
+            Opcode::Equal => {
+                let [a, b, c] = args;
+                if self.memory[a as usize] == self.memory[b as usize] {
+                    self.memory[c as usize] = 1;
+                } else {
+                    self.memory[c as usize] = 0;
+                }
+                Ok(Status::Continue)
+            }
+            Opcode::Halt => Ok(Status::Halt),
+        }
+    }
+
+    fn run(&mut self) -> Result<isize> {
+        loop {
+            match self.step()? {
+                Status::Continue => {}
+                Status::Halt => break Ok(self.memory[0]),
+            }
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -72,98 +167,5 @@ impl TryFrom<isize> for Instruction {
                 (((n / 10000) % 10) as u8).try_into()?,
             ],
         })
-    }
-}
-
-impl Machine {
-    fn step(&mut self) -> Result<Status> {
-        let instr = Instruction::try_from(self.memory[self.pc])?;
-
-        // Create arguments
-        let mut args = [
-            self.pc as isize + 1,
-            self.pc as isize + 2,
-            self.pc as isize + 3,
-        ];
-        args[..instr.opcode.arg_count()]
-            .iter_mut()
-            .zip(instr.modes.iter())
-            .for_each(|(arg, mode)| match mode {
-                Mode::Position => *arg = self.memory[*arg as usize],
-                Mode::Immidiate => {}
-            });
-
-        // Update program counter
-        self.pc += instr.opcode.arg_count() + 1;
-
-        match instr.opcode {
-            Opcode::Add => {
-                let [a, b, c] = args;
-                self.memory[c as usize] = self.memory[a as usize] + self.memory[b as usize];
-                Ok(Status::Continue)
-            }
-            Opcode::Mul => {
-                let [a, b, c] = args;
-                self.memory[c as usize] = self.memory[a as usize] * self.memory[b as usize];
-                Ok(Status::Continue)
-            }
-            Opcode::Input => {
-                let [a, _, _] = args;
-                self.memory[a as usize] = {
-                    let mut s = String::new();
-                    std::io::stdin().read_line(&mut s)?;
-                    s.trim().parse()?
-                };
-                Ok(Status::Continue)
-            }
-            Opcode::Output => {
-                let [a, _, _] = args;
-                println!("{}", self.memory[a as usize]);
-                std::io::stdout().flush()?;
-                Ok(Status::Continue)
-            }
-            Opcode::IsNotZero => {
-                let [a, b, _] = args;
-                if self.memory[a as usize] != 0 {
-                    self.pc = self.memory[b as usize] as usize;
-                }
-                Ok(Status::Continue)
-            }
-            Opcode::IsZero => {
-                let [a, b, _] = args;
-                if self.memory[a as usize] == 0 {
-                    self.pc = self.memory[b as usize] as usize;
-                }
-                Ok(Status::Continue)
-            }
-            Opcode::LessThan => {
-                let [a, b, c] = args;
-                if self.memory[a as usize] < self.memory[b as usize] {
-                    self.memory[c as usize] = 1;
-                } else {
-                    self.memory[c as usize] = 0;
-                }
-                Ok(Status::Continue)
-            }
-            Opcode::Equal => {
-                let [a, b, c] = args;
-                if self.memory[a as usize] == self.memory[b as usize] {
-                    self.memory[c as usize] = 1;
-                } else {
-                    self.memory[c as usize] = 0;
-                }
-                Ok(Status::Continue)
-            }
-            Opcode::Halt => Ok(Status::Halt),
-        }
-    }
-
-    fn run(&mut self) -> Result<isize> {
-        loop {
-            match self.step()? {
-                Status::Continue => {}
-                Status::Halt => break Ok(self.memory[0]),
-            }
-        }
     }
 }
