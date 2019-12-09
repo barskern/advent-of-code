22d1acfaf06339ad4fc1006699ba67ef36cf24ed
Ole Martin Ruud <barskern@outlook.com>
2019-12-09 15:02:04 +0100

    Solve day 9 part 2

--- src/day9.rs
+++ src/day9.rs
@@ -10,11 +10,12 @@ pub fn part1(input: &str) -> Result<isize> {
     let memory: Vec<isize> = input.split(',').map(|s| s.parse().unwrap()).collect();
 
     let (input_tx, input_rx) = sync_channel::<isize>(4);
-    let _ = input_tx.send(1);
-
     let (output_tx, output_rx) = sync_channel::<isize>(10);
 
     let mut machine = Machine::new(memory, input_rx, output_tx);
+
+    // Run in test mode
+    let _ = input_tx.send(1);
     machine.run()?;
 
     let results: Vec<_> = output_rx.try_iter().collect();
@@ -25,10 +26,26 @@ pub fn part1(input: &str) -> Result<isize> {
     Ok(*results.last().unwrap())
 }
 
-// #[aoc(day9, part2)]
-// pub fn part2(input: &str) -> Result<usize> {
-//     unimplemented!()
-// }
+#[aoc(day9, part2)]
+pub fn part2(input: &str) -> Result<isize> {
+    let memory: Vec<isize> = input.split(',').map(|s| s.parse().unwrap()).collect();
+
+    let (input_tx, input_rx) = sync_channel::<isize>(4);
+    let (output_tx, output_rx) = sync_channel::<isize>(4);
+
+    let mut machine = Machine::new(memory, input_rx, output_tx);
+
+    // Run in boost mode
+    let _ = input_tx.send(2);
+    machine.run()?;
+
+    let results: Vec<_> = output_rx.try_iter().collect();
+    if results.len() != 1 {
+        return Err(format!("failed running opcodes: {:?}", results).into());
+    }
+
+    Ok(*results.last().unwrap())
+}
 
 #[cfg(test)]
 mod tests {}
