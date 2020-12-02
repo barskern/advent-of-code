580247d2a76139543a4db79dc0566bf17f557521
Ole Martin Ruud <dev@barskern.no>
2020-12-02 06:22:25 +0100

    Solve day 2

---
+++ 2020/src/day2.rs
@@ -0,0 +1,33 @@
+use aoc_runner_derive::*;
+
+use anyhow::{Context, Result};
+
+#[aoc(day2, part1)]
+pub fn part1(input: &str) -> Result<usize> {
+    fn compliant_password(s: &str) -> bool {
+        let (lower, upper, letter, password): (usize, usize, char, &str) =
+            serde_scan::scan!("{}-{} {}: {}" <- s).unwrap();
+
+        let matches = password.matches(letter).count();
+
+        lower <= matches && matches <= upper
+    }
+
+    Ok(input.lines().filter(|s| compliant_password(s)).count())
+}
+
+#[aoc(day2, part2)]
+pub fn part2(input: &str) -> Result<usize> {
+    fn compliant_password(s: &str) -> bool {
+        let (one, two, letter, password): (usize, usize, char, &str) =
+            serde_scan::scan!("{}-{} {}: {}" <- s).unwrap();
+
+        (password.chars().nth(one - 1).unwrap() == letter)
+            ^ (password.chars().nth(two - 1).unwrap() == letter)
+    }
+
+    Ok(input.lines().filter(|s| compliant_password(s)).count())
+}
+
+#[cfg(test)]
+mod tests {}
--- 2020/src/lib.rs
+++ 2020/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod day2;
 pub mod day1;
 
 aoc_runner_derive::aoc_lib! { year = 2020 }
